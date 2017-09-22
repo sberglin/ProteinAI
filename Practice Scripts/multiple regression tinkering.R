b3e4563c1208dda67377acadce96e9856b70050e
sberglin <berglin@wisc.edu>
2017-09-22 14:17:57 -0500

    9/22

--- Practice Scripts/multiple regression tinkering.R
+++ Practice Scripts/multiple regression tinkering.R
@@ -7,9 +7,3 @@ y = 3 + 4*x1 + 5*x2 + 6*x3 + 7*x1*x2 + rnorm(n)
 m = lm(y ~ x1 + x2 + x3 + x1*x2)
 # Displaying Model
 print(summary(m))
-
-
-# Using mtcars data
-# m3 = lm(mpg ~ hp + wt + gear, data=mtcars)
-# summary(m3)
-# anova(m3)
--- prelim_data tinkering.R
+++
@@ -1,9 +0,0 @@
-# Removing Environmental Variables
-rm(list = ls())
-
-# Reading Data
-data = read.csv("prelim_data.txt", header = FALSE,
-                col.names = c("Features", "Functionality"),
-                colClasses = c("character", "integer"))
-# Converting Binary Functionality Vector to Logical
-data$Functionality = as.logical(data$Functionality)
