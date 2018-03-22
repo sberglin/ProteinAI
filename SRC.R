544dc0aa9eb201196eab8a4a68a435d884be3c54
sberglin <berglin@wisc.edu>
2018-03-22 12:32:11 -0500

    d

--- SRC.R
+++ SRC.R
@@ -2,6 +2,6 @@ library(randomForestSRC)
 
 # Loading Data
 source("functions/load_protein_data.R")
-p450 = load("P450/block/enzyme.txt")
+p450 = load("P450/block/enzyme.txt") 
 
 rfsrc = rfsrc(Functionality ~ ., p450)
